8f4b2cdff3c7dc2a206a82085760f53a586a07cd
Lanselord <nosirovhumoyiddin23@gmail.com>
2024-02-06 15:40:41 +1000

    React_tayyori

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+// JSX
+// class => className
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="container">
+          
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
